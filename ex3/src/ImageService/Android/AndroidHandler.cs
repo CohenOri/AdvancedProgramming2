b1fdc8625634283b8c10434228f675382b7bdcec
CohenOri <oricohen70@gmail.com>
2018-06-21 15:25:54 +0300

    Fixes for android handler (client)

--- ex3/src/ImageService/Android/AndroidHandler.cs
+++ ex3/src/ImageService/Android/AndroidHandler.cs
@@ -11,8 +11,8 @@ namespace ImageService.Android
 {
     class AndroidHandler
     {
-       private string DirectoryPath;
-       public AndroidHandler(string path)
+        private string DirectoryPath;
+        public AndroidHandler(string path)
         {
             DirectoryPath = path;
         }
@@ -21,34 +21,32 @@ namespace ImageService.Android
             //create a task witch will listen to  client command and execute them.
             new Task(() =>
             {
-            try
-            {
-                NetworkStream stream = client.GetStream();
-                BinaryReader reader = new BinaryReader(stream);
-
-                string data = "";
-                byte[] name;
-                int type = 0;
-                byte[] image = null;
-                type = reader.Read();
-                image = reader.ReadBytes(type);
-                while (type != 1)
+                try
                 {
-                    type = reader.Read();
-                    //read the type message
-                    if (type == 0 || type == 1) break;
-                    //reade image byte array
-                    image = reader.ReadBytes(type);
-                    //reade name
-                    type = reader.Read();
-                    //read the type message
-                     if (type == 0 || type == 1) break;
-                    //reade image byte array
-                    name = reader.ReadBytes(type);
-                     MovePicture(image, name);
+                    NetworkStream stream = client.GetStream();
+                    BinaryReader reader = new BinaryReader(stream);
+                    int picLengthInBytes = 0;
+                    int picNameLengthInBytes = 0;
+                    byte[] imageByteArr = null;
+                    byte[] name;
+                    picLengthInBytes = reader.ReadInt32(); // read length
+                    picNameLengthInBytes = reader.ReadInt32(); // read pic Name length
+
+                    int numBytesRead = 0;
+                    while (picLengthInBytes > 0) // read the client's image byte arr to imageByteArr
+                    {
+                        // Read may return anything from 0 to numBytesToRead.
+                        int n = reader.Read(imageByteArr, numBytesRead, picLengthInBytes);
+
+                        // Break when the end of the file is reached.
+                        if (n == 0) { break; }
+                        numBytesRead += n;
+                        picLengthInBytes -= n;
                     }
-                    //if client closed
-                    client.Close();
+
+                    name = reader.ReadBytes(picNameLengthInBytes); // read pic Name
+                    MovePicture(imageByteArr, name);
+                    //if client closed       client.Close();
                 }
                 catch (Exception)
                 {
@@ -60,8 +58,8 @@ namespace ImageService.Android
         private void MovePicture(byte[] info, byte[] fileName)
         {
             Image a = ByteArrayToImage(info);
-            a.Save(this.DirectoryPath + "\\" + GetString(fileName));        
-            
+            a.Save(this.DirectoryPath + "\\" + GetString(fileName));
+
         }
 
         private string GetString(byte[] bytes)
